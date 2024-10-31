25ec3f411b2ba25eb92a3876465c72c39b74e443
EarthSR <earthsrichok31@gmail.com>
2024-11-01 01:43:35 +0700

    update

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import ManagePosts from './component/managepost';
 import ManageReportedPosts from './component/ManageReports';
 import ManageCategories from './component/ManageCategories';
 import PrivacyPolicy from './component/PrivacyPolicy';
+import AccountDeletion from './component/AccountDeletion';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/manage-reported-posts" element={<ManageReportedPosts />} />
           <Route path="/managecategories" element={<ManageCategories/>}/>
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/account-deletion" element={<AccountDeletion />} />
         </Routes>
       </AuthProvider>
     </Router>
